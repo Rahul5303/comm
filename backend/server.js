05273259ac43f0a4f081dca25c9c464a424f4223
Rahul5303 <rahulnishad977@gmail.com>
2023-05-28 01:36:22 +0530

    srver

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+
+
+// express
+const express=require("express");
+// connection for config
+const {connection} =require("./config/db");
+// product route from router
+// cors
+const cors=require("cors");
+
+const ProductRoute=require("./routes/product.route");
+
+const app=express();
+
+app.use(express.json());
+
+// cors
+
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+    })
+  );
+
+
+// products route
+
+app.use("/products",ProductRoute);
+
+
+app.get("/",(req,res)=>{
+    res.send("HomePage");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("AboutPage");
+})
+
+app.listen(8001,async()=>{
+    try{
+        await connection
+        console.log("Connected to DB Successfully")
+    }
+    catch(err){
+        console.log("Connection failed");
+        console.log(err);
+    }
+    console.log("Listening to Port 8001");
+})
